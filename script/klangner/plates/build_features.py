61f059bcbcacb705e0c88ee04113537bd96ae95b
klangner <klangner@gmail.com>
2016-11-09 06:43:40 -0800

    Build traning set based on inception model

--- script/klangner/plates/build_features.py
+++ script/klangner/plates/build_features.py
@@ -30,17 +30,20 @@ if __name__ == "__main__":
     # Prepare inception graph
     graph = TensorFlowGraph()
     # Load all images from folder
+    start_index = 0
+    batch_size = 5000
+    end_index = start_index+batch_size
     start_time = time.time()
     folder = IMAGE_PATH + 'positives/'
-    features = [graph.parse_image(folder + fp) for fp in os.listdir(folder)]
+    features = [graph.parse_image(folder + fp) for fp in os.listdir(folder)[start_index:end_index]]
     df_positives = pd.DataFrame(features)
     df_positives['label'] = 1
     folder = IMAGE_PATH + 'negatives/'
-    features = [graph.parse_image(folder + fp) for fp in os.listdir(folder)]
+    features = [graph.parse_image(folder + fp) for fp in os.listdir(folder)[start_index:end_index]]
     df_negatives = pd.DataFrame(features)
     df_negatives['label'] = 0
     df = df_positives.append(df_negatives)
     print(df.head())
     total_time = time.time() - start_time
-    print('Parsed {} images in {} seconds'.format(len(features), total_time))
-    df.to_csv('../../../data/classifier/train.csv')
+    print('Parsed {} images in {} seconds'.format(len(df), total_time))
+    df.to_csv('../../../data/classifier/train-{}.csv'.format(start_index))
